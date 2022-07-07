ff5e291a81397035d82321583f503b807f46fb89
백범진 <107093406+bbj0504@users.noreply.github.com>
2022-07-07 11:21:34 +0900

    Update study05.py

--- python/Python_normal/study05.py
+++ python/Python_normal/study05.py
@@ -23,3 +23,42 @@ print(n * 100, n.__mul__(100))
 
 print()
 print()
+
+# 클래스 예제1
+class Fruit:
+    def __init__(self, name, price):
+        self._name = name
+        self._price = price
+
+    def __str__(self):
+        return 'Fruit Class Info : {} , {}'.format(self._name, self._price)
+
+    def __ge__(self, x):
+        print('Called >> __ge__ Method.')
+        if self._price >= x._price:
+            return True
+        else:
+            return False
+
+    def __le__(self, x):
+        print('Called >> __le__ Method.')
+        if self._price <= x._price:
+            return True
+        else:
+            return False
+
+    def __sub__(self, x):
+        print('Called >> __sub__ Method.')
+        return self._price - x._price
+
+# 인스턴스 생성
+s1 = Fruit('Orange', 7500)
+s2 = Fruit('Banana', 3000)
+
+# 매직메소드 출력
+print(s1 >= s2)
+print(s1 <= s2)
+print(s1 - s2)
+print(s2 - s1)
+print(s1)
+print(s2)
